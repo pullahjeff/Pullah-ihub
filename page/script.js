70e37086ca45ce266916b431a7a97e3406979fd6
benaebipullah <benaebipullah2017@gmail.com>
2021-06-25 11:29:59 +0100

    Index.html

---
+++ page/script.js
@@ -0,0 +1,52 @@
+//function myFunction(){
+   // document.getElementById("demo").innerHTML = "the paragraph change"
+    //window.alert("frontend")
+   // document.write("Jeffrey")
+   // console.log(900) 
+    //declare variable
+    //var x = 5
+   // var y = 6
+   // var z = x+y
+    //console
+   // var name="name";
+   // var person ="number";
+   // var address ="boolean";
+   // var rightwrong="null";
+   // var numberchildren="nundefined";
+
+    //console
+   // console.log("name")
+   // conswole.log("number")
+   // console.log("boolean")
+  //  console.log("null")
+  //  console.log("nundefind")
+
+    //variable declearation
+    var A =2;
+    var B =2;
+    var C =2;
+    var multiple =A*B*C;
+
+    console.log(multiple);
+
+    //variable declearation
+    var A=2;
+    var B=2;
+    var C=2;
+    var div =A/C;
+
+    //console
+    console.log(div);
+
+    //variable declearation
+    var A=8;
+    var B=2;
+    var C=2;
+    var subt =A-B-C;
+    
+    //console
+    console.log(subt);
+    //var declear
+    var b =6;
+    b++;
+  console.log(b);
